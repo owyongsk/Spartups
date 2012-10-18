63a5674ae910d74217a6620961ed22ac43f55e01
SoonKhen OwYong <dude@owyong.sk>
2012-10-18 13:09:27 -0700

    changed the layout, only logo left to go

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
+  def home
+    @title = "Spartups"
+  end
+
   def about
     @title = "About Spartups"
   end
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Spartups::Application.routes.draw do
   resources :projects
   resources :relations
 
-  root :to => 'profiles#index'
+  root :to => 'pages#home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
